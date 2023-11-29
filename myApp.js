ca0b11e6fc594e35e0d00400fd13ee35e1fe27b9
olaniyanpelumi <olaniyanp11@gmail.com>
2023-11-29 20:41:55 +0100

    serve json files  based on .env file

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
   res.sendFile(absolute);
 });
 app.get("/json", (req, res) => {
-  let mess = process.env.MESSAGE_STYLE;
+  let mess = process.env.MESSAGE_STYLE.toLowerCase();
   console.log(mess);
   if (mess === "uppercase") {
     res.json({ message: "HELLO JSON" });
