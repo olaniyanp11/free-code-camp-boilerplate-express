6bb12bb1bf3b7762a08fc14675e741668f099ec8
olaniyanpelumi <olaniyanp11@gmail.com>
2023-11-30 00:53:19 +0100

    collected reqests from params

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-let auth = require('./middleware/auth')
+let auth = require("./middleware/auth");
 
 let express = require("express");
 let app = express();
 app.use("/public", express.static(__dirname + "/public"));
-app.use(auth)
+app.use(auth);
 
 app.get("/", (req, res) => {
   let absolute = __dirname + "/views/index.html";
@@ -16,12 +16,20 @@ app.get("/json", (req, res) => {
     res.json({ message: "HELLO JSON" });
   } else res.json({ message: "Hello json" });
 });
-app.get("/now", (req, res,next) => {
-    let time = new Date().toString()
-    req.time = time
-    next()
-}, function(req,res){
-    res.json({time: req.time})
-})
+app.get(
+  "/now",
+  (req, res, next) => {
+    let time = new Date().toString();
+    req.time = time;
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+app.get("/:word/echo", (req, res) => {
+    let word = req.params.word
+    res.json({echo : word})
+});
 
 module.exports = app;
