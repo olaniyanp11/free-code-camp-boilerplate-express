b26317bd2ad7d475af5f51a931c67aac07af53db
olaniyanpelumi <olaniyanp11@gmail.com>
2023-11-29 20:06:57 +0100

    configured static and rendered html file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
-console.log("Hello World")
+app.use('/public',express.static(__dirname + "/public"))
+
 app.get('/', (req, res) => {
-    res.send("Hello Express")
+    let absolute = __dirname + "/views/index.html";
+    res.sendFile(absolute)
 })
 
  module.exports = app;
