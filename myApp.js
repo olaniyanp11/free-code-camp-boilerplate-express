ab974054b0566a9a523154491a2ddde018826f74
olaniyanpelumi <olaniyanp11@gmail.com>
2023-11-29 19:41:30 +0100

    using app.get() method to serve the string Hello Express' to GET requests

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
 let express = require('express');
 let app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("hello")
+app.get('/', (req, res) => {
+    res.send("Hello Express")
+})
 
  module.exports = app;
